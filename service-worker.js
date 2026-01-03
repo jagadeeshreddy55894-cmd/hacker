b0d5079ea92f2cb1a1a4ece439c8a5f9f930f993
jagadeeshreddy55894-cmd <jagadeeshreddy55894@gmail.com>
2026-01-03 20:37:41 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "mess-coupon-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./profile.jpg",
+  "./video.mp4"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
